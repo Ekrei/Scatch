90f0297807dad546a0788dbe01bcf5dc01a6c166
Izus_X <vanechec.c@gmail.com>
2024-12-02 20:37:58 +1000

    delete wrong files

--- src/database/Models/BusketModel.ts
+++
@@ -1,9 +0,0 @@
-import {DataTypes} from "sequelize"
-import Sequelize from "../"
-
-const BusketModel = Sequelize.define("comment",{
-    userId: {type: DataTypes.INTEGER},
-    productId: {type: DataTypes.INTEGER}
-})
-
-export default BusketModel
